c7eaeffe37d8a4ef41d0ff8489db65e578f53c02
서희석 <49949019+Basic-Developer@users.noreply.github.com>
2020-02-12 15:35:28 +0900

    Add files via upload

---
+++ 나는 요리사다_0212/shs_iamcook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz
+{
+
+    class Quiz_3
+    {
+
+        private int Max = 0, Sum = 0, Winner = 0;
+        private string[] Score;
+        private string Data;
+
+        public void Input() { CheckWinner(); }
+
+
+        private void CheckWinner()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Data = Console.ReadLine();
+
+                Score = Data.Split(' ');
+
+                for (int j = 0; j < Score.Length; Sum += Int32.Parse(Score[j++])) ;
+
+                if (Max < Sum)
+                {
+                    Max = Sum;
+                    Winner = i + 1;
+                }
+                Sum = 0;
+            }
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("{0} {1}", Winner, Max);
+        }
+    }
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Quiz_3 quiz3 = new Quiz_3();
+            quiz3.Input();
+            quiz3.Result();
+        }
+
+    }
+}
